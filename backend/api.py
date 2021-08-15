2e5c551d3f9de8bbbaaefccb8ed8a044660f2df8
Kirill K <kovalev.kirill.a@gmail.com>
2021-08-15 18:32:51 +0300

    feat: add account

--- backend/api.py
+++ backend/api.py
@@ -1,15 +1,22 @@
 from flask import Flask
 from flask_restplus import Resource, Api
+from model.account import Account
 from model.person import Person
 
 app = Flask(__name__)
 api = Api(app)
 
 
-@api.route("/person/<string:person>")
+@api.route("/account/<string:account_id>")
+class HandlerAccount(Resource):
+    def get(self, account_id):
+        return Account(id=account_id).__dict__
+
+
+@api.route("/person/<string:person_id>")
 class HandlerPerson(Resource):
-    def get(self, person):
-        return Person(id=person).__dict__
+    def get(self, person_id):
+        return Person(id=person_id).__dict__
 
 
 if __name__ == "__main__":
---
+++ backend/model/account.py
@@ -0,0 +1,8 @@
+class Account(object):
+    """Account."""
+
+    def __init__(self, **kwargs):
+        self.id = kwargs.get("id")
+
+        self.email = None
+        self.password = None
